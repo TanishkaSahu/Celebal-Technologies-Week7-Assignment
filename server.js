b94a69e7a7d631ce732b6f596a39db9c6f3f1e0b
TanishkaSahu <tanishkasahu001@gmail.com>
2025-07-20 18:01:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
